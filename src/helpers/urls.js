2d91599515e4c783dc033d5ae953ec3ee3a78090
tanuchoudhary21 <chtaruna90@gmail.com>
2021-05-28 23:00:04 +0530

    User Login 2

---
+++ src/helpers/urls.js
@@ -0,0 +1,7 @@
+const API_ROOT = 'http://codeial.codingninjas.com:8000/api/v2';
+
+export const APIUrls = {
+  login: () => `${API_ROOT}/users/login`,
+  signup: () => `${API_ROOT}/users/signup`,
+  fetchPosts: (page = 1, limit = 7) => `${API_ROOT}/posts?page=${page}&limit=${limit}`,
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,5 +4,5 @@ import auth from './auth';
 
 export default combineReducers({
     posts,
-    
+    auth
 });
