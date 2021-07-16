43423c82eb01014489b186669976a6e2acf23aa3
tanuchoudhary21 <chtaruna90@gmail.com>
2021-07-16 14:05:08 +0530

    Updated signup.jsx

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -64,7 +64,7 @@ class Signup extends Component {
         </div>
         <div className="field">
           <input
-            placeholder="Confirm password"
+            placeholder="Password"
             type="password"
             required
             onChange={(e) =>
@@ -74,7 +74,7 @@ class Signup extends Component {
         </div>
         <div className="field">
           <input
-            placeholder="Password"
+            placeholder="Confirm Password"
             type="password"
             required
             onChange={(e) => this.handleInputChange('password', e.target.value)}
